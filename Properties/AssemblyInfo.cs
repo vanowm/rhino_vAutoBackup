722008072afadc8ec33afb13829c05862f95d6b4
V@no <git@vano.org>
2026-05-06 11:11:56 -0400

    ﻿26.05.06.120000: build: sync version metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,20 @@
 using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
 
 [assembly: AssemblyTitle("vAutoBackup")]
 [assembly: AssemblyDescription("Rhino periodic auto-backup plug-in")]
 [assembly: AssemblyProduct("vAutoBackup")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f8e4d2c1-3b7a-4e5f-9d0c-6a2b8e7f1c4d")]
 [assembly: AssemblyVersion("26.5.6.1200")]
 [assembly: AssemblyFileVersion("26.5.6.1200")]
 [assembly: AssemblyInformationalVersion("26.05.06.120000")]
+
+[assembly: PlugInDescription(DescriptionType.Organization, "")]
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
